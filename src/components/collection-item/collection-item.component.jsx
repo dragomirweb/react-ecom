c05a2cfe000012ed32dda5aeebaa1f755c0b6a74
DDR <dan@dragomirweb.com>
2020-04-20 16:51:28 +0300

    refactor formating

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 const CollectionItem = ({ id, price, imageUrl, name }) => (
-	<div className="col-span-3 min-h-xl flex flex-col">
-		<div
-			className="bg-cover bg-center bg-no-repeat min-h-xl w-full"
-			style={{
-				backgroundImage: `url(${imageUrl})`
-			}}
+  <div className="col-span-12 md:col-span-6 lg:col-span-3 min-h-xl flex flex-col">
+    <div
+      className="bg-cover bg-center bg-no-repeat min-h-xl w-full"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
     />
     <div className="flex justify-between text-xl mt-3">
-    <span className="font-medium">{name}</span>
-    <span className="font-light">${price}</span>
+      <span className="font-medium">{name}</span>
+      <span className="font-light">${price}</span>
     </div>
-	</div>
+  </div>
 );
 
 export default CollectionItem;
