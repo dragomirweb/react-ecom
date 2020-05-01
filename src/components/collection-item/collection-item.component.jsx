a60744abc7fe5a9becbf7932eba33ab4fe7162b9
DDR <dan@dragomirweb.com>
2020-05-01 14:46:08 +0300

    updated shop date with new data, updated cart uitils to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
 
-const CollectionItem = ({ id, price, imageUrl, name }) => (
-  <div className="col-span-12 md:col-span-6 lg:col-span-3 min-h-xl flex flex-col">
-    <div
-      className="bg-cover bg-center bg-no-repeat min-h-xl w-full"
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className="flex justify-between text-xl mt-3">
-      <span className="font-medium">{name}</span>
-      <span className="font-light">${price}</span>
-    </div>
-  </div>
-);
+import './collection-item.styles.scss';
+import { addItem } from '../../redux/cart/cart.actions';
 
-export default CollectionItem;
+const CollectionItem = ({ item, addItem }) => {
+	const { name, price, imageUrl } = item;
+
+	return (
+		<div className="cart-item col-span-12 md:col-span-6 lg:col-span-3 min-h-xl flex flex-col relative">
+			<div
+				className="bg-cover bg-center bg-no-repeat min-h-xl w-full"
+				style={{
+					backgroundImage: `url(${imageUrl})`
+				}}
+			/>
+			<div className="flex justify-between text-xl mt-3">
+				<span className="font-medium">{name}</span>
+				<span className="font-light">${price}</span>
+			</div>
+			<div className="flex justify-center absolute inset-0" style={{ top: '20rem' }}>
+				<CustomButton inverted onClick={() => addItem(item)}>
+					Add to cart
+				</CustomButton>
+			</div>
+		</div>
+	);
+};
+
+const mapDispatchToProps = (dispatch) => ({
+	addItem: (item) => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,7 +7,7 @@ const CollectionPreview = ({ title, items }) => (
 		<h1 className="text-4xl mb-5 mt-10">{title.toUpperCase()}</h1>
 
 		<div className="grid grid-cols-12 gap-6">
-			{items.filter((item, i) => i < 4).map((item, index) => <CollectionItem key={index} {...item} />)}
+			{items.filter((item, i) => i < 4).map((item, index) => <CollectionItem key={index} item={item} />)}
 		</div>
 	</div>
 );
