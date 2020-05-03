1b56383eb990448a30d23306fba493b913e8f8d4
Dragomir Dan <dan@dragomirweb.com>
2020-05-03 22:56:39 +0300

    refactor styles

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,7 +5,9 @@ import './cart-item.styles.scss'
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
   return (
     <div className="cart-item">
-      <img src={imageUrl} alt="item" />
+      <div className="relative w-1/3 pb-1/3">
+      <img className="absolute w-full h-full object-cover" src={imageUrl} alt="item" />
+      </div>
       <div className="item-details">
         <span className="name">{name}</span>
         <span className="price">
