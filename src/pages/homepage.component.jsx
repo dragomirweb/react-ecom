4d075e36d62dc05eef4baea6340569e49578f3e7
Dragomir Dan <dan@dragomirweb.com>
2020-05-03 11:53:06 +0300

    add styled components

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-import './homepage.styles.scss'
+
 import Directory from '../components/directory/directory.component'
 
-const HomePage = () => <Directory />
+import { HomePageContainer } from './homepage.styles'
+import './homepage.styles.scss'
 
+const HomePage = () => (
+  <HomePageContainer>
+    <Directory />
+  </HomePageContainer>
+)
 export default HomePage
