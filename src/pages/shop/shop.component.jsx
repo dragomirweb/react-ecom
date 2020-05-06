57787cee0d90fd6e1687b7b97a65ba69f8f782f6
Dragomir Dan <dan@dragomirweb.com>
2020-05-06 21:24:56 +0300

    add spinner

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,12 +5,20 @@ import { connect } from 'react-redux'
 import { updateCollections } from '../../redux/shop/shop.actions'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 import CollectionPage from '../collection/collection.component'
+import WithSpinner from '../../components/with-spinner/with-spinner.component'
 import {
   firestore,
   convertCollectionsSnapshotToMap
 } from '../../firebase/firebase.utils'
 
+const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
+
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  }
+
   unsubscribeFromSnapshot = null
 
   componentDidMount () {
@@ -20,18 +28,20 @@ class ShopPage extends React.Component {
     collectionRef.onSnapshot(async (snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
       updateCollections(collectionsMap)
+      this.setState({ loading: false })
     })
   }
 
   render () {
     const { match } = this.props
+    const {loading} = this.state
 
     return (
       <div>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={loading} {...props} />} />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />}
         />
       </div>
     )
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -13,13 +13,14 @@ export const selectCollectionsForPreview = createSelector(
   [
     selectCollections
   ],
-  (collections) => Object.keys(collections).map((key) => collections[key])
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
 )
 
 export const selectCollection = (collectionUrlParam) =>
   createSelector(
     [
-      selectCollections,
+      selectCollections
     ],
-    (collections) => collections[collectionUrlParam]
+    (collections) => collections ? collections[collectionUrlParam] : null
   )
